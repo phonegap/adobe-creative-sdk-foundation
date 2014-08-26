b37d0dbae2d19d532cffdd5c23af771ef85f6290
Shazron Abdullah <shazron@apache.org>
2014-08-25 17:37:14 -0700

    Fixes #5 - Update Login according to Getting Started (cached login)

--- src/ios/PGAdobeCreativeSDKFoundation.m
+++ src/ios/PGAdobeCreativeSDKFoundation.m
@@ -5,14 +5,9 @@
 
 @implementation PGAdobeCreativeSDKFoundation
 
-#define CLIENT_ID       @""
-#define CLIENT_SECRET   @""
+#define ADOBE_CSDK_CLIENT_ID       @""
+#define ADOBE_CSDK_CLIENT_SECRET   @""
 
-
-- (id)settingForKey:(NSString*)key
-{
-    return [self.commandDelegate.settings objectForKey:[key lowercaseString]];
-}
 
 - (void)pluginInitialize
 {
@@ -20,29 +15,41 @@
     // hard-code this. We do NOT want to expose the CLIENT_SECRET in JavaScript, it has to be in native code.
     // This could be a post-build hook
     
-    if ([@"" isEqualToString:CLIENT_ID]) {
-        NSLog(@"WARNING - PGAdobeCreativeSDK - CLIENT_ID is not set");
+    if ([@"" isEqualToString:ADOBE_CSDK_CLIENT_ID]) {
+        NSLog(@"WARNING - PGAdobeCreativeSDKFoundation - ADOBE_CSDK_CLIENT_ID is not set");
     }
-    if ([@"" isEqualToString:CLIENT_SECRET]) {
-        NSLog(@"WARNING - PGAdobeCreativeSDK - CLIENT_SECRET is not set");
+    if ([@"" isEqualToString:ADOBE_CSDK_CLIENT_SECRET]) {
+        NSLog(@"WARNING - PGAdobeCreativeSDKFoundation - ADOBE_CSDK_CLIENT_SECRET is not set");
     }
     
-    [[AdobeUXAuthManager sharedManager] setAuthenticationParametersWithClientID:CLIENT_ID withClientSecret:CLIENT_SECRET];
+    [[AdobeUXAuthManager sharedManager] setAuthenticationParametersWithClientID:ADOBE_CSDK_CLIENT_ID withClientSecret:ADOBE_CSDK_CLIENT_SECRET];
 }
 
 - (void) login:(CDVInvokedUrlCommand*)command
 {
     __weak CDVPlugin* weakSelf = self;
     
-	[[AdobeUXAuthManager sharedManager] login:self.viewController
-     onSuccess:^(AdobeAuthUserProfile* profile) {
-         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
-                                                       messageAsDictionary:[profile propertiesAsDictionary]];
-         [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     } onError:^(NSError *error) {
-         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
-         [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-     }];
+    void(^loginSuccess)(AdobeAuthUserProfile*)= ^(AdobeAuthUserProfile* profile) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                      messageAsDictionary:[profile propertiesAsDictionary]];
+        [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    };
+    
+    void(^loginFailure)(NSString*)= ^(NSString* errorMessage) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+        [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    };
+    
+    if([AdobeUXAuthManager sharedManager].authenticated) {
+        loginSuccess([AdobeUXAuthManager sharedManager].userProfile);
+    } else {
+        [[AdobeUXAuthManager sharedManager] login:self.viewController
+         onSuccess:^(AdobeAuthUserProfile* profile) {
+             loginSuccess(profile);
+         } onError:^(NSError *error) {
+             loginFailure([error localizedDescription]);
+         }];
+    }
 }
 
 - (void) logout:(CDVInvokedUrlCommand*)command
