fe574251a10fe22919bcc383e499e8a288e5bede
Shazron Abdullah <shazron@apache.org>
2016-06-07 15:34:31 -0700

    Removed source code, updated README to point to CreativeSDK github org.

--- src/ios/NSArray+ItemAsDictionary.h
+++
@@ -1,9 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#import <AdobeCreativeSDKFoundation/AdobeAsset.h>
-
-@interface NSArray (ItemAsDictionary)
-
-- (NSArray*) arrayWithItemsAsDictionaries;
-
-@end
--- src/ios/NSObject+PropertiesAsDictionary.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface NSObject (PropertiesAsDictionary)
-
-- (NSDictionary *) propertiesAsDictionary;
-
-@end
